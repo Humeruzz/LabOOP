efdb629edd2fb5dc0029a484c50184c89f58b9a9
Alexandr <humeruzz0@gmail.com>
2019-10-08 00:55:01 +0300

    08.10.19 1

--- src/MainWindow/Controller/MainMenuController.java
+++ src/MainWindow/Controller/MainMenuController.java
@@ -1,5 +1,8 @@
 package MainWindow.Controller;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,10 +10,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 
 public class MainMenuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private Button ButtonLab1;
@@ -19,17 +26,15 @@ public class MainMenuController {
     void initialize() {
         ButtonLab1.setOnAction(actionEvent -> {
             ButtonLab1.getScene().getWindow().hide();
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("Visual/Lab1.fxml"));
             try {
-                loader.load();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../Visual/Lab1.fxml"));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root,696, 430));
+                stage.showAndWait();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Parent root = loader.getRoot();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root,696, 430));
-            stage.showAndWait();
         });
     }
 
--- src/MainWindow/Main.java
+++ src/MainWindow/Main.java
@@ -1,6 +1,5 @@
 package MainWindow;
 
-import MainWindow.Controller.Lab1Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
